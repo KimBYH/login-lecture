1aacbd9e42a357a9b126f1d9157fcb9d6f116680
unknown <alxnfkfkfk@naver.com>
2025-01-27 16:03:34 +0900

    MVC의 모델 (M) 만들기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,9 +1,6 @@
 "use strict";
 
-const users = {
-  id: ["1234", "김", "kim"],
-  pwd: ["1111", "1234", "123456"],
-};
+const UserStorage = require("../../models/UserStorage");
 
 const output = {
   home: (req, res) => {
@@ -19,19 +16,21 @@ const process = {
     const id = req.body.id,
       pwd = req.body.pwd;
 
+    const users = UserStorage.getUsers("id", "pwd");
+
+    const response = {};
+
     if (users.id.includes(id)) {
       const idx = users.id.indexOf(id);
       if (users.pwd[idx] === pwd) {
-        return res.json({
-          success: true,
-        });
+        response.success = true;
+        return res.json(response);
       }
     }
 
-    return res.json({
-      success: false,
-      msg: "로그인에 실패하셨습니다.",
-    });
+    response.success = false;
+    response.msg = "로그인에 실패 했습니다.";
+    return res.json(response);
   },
 };
 
